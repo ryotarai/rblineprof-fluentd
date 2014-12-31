ddb884f8df2fcb94c550369ab382df01e470ac5c
Ryota Arai <ryota.arai@gmail.com>
2014-12-31 23:02:16 +0900

    bundle gem.

---
+++ lib/rblineprof/fluentd.rb
@@ -0,0 +1,7 @@
+require "rblineprof/fluentd/version"
+
+module Rblineprof
+  module Fluentd
+    # Your code goes here...
+  end
+end
---
+++ lib/rblineprof/fluentd/version.rb
@@ -0,0 +1,5 @@
+module Rblineprof
+  module Fluentd
+    VERSION = "0.0.1"
+  end
+end
